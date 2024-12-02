617c3fd98516d92d266f36e14eb75e795956dc11
Ertenox <auguste.mendy@etu.imt-nord-europe.fr>
2024-12-02 08:57:56 +0100

    Déplacement de fichier

--- src/main/java/com/imt/projet/Banque/infrastructure/events/Contrats/ApplyInterestEventPublisher.java
+++ src/main/java/com/imt/projet/Banque/infrastructure/events/Contrats/ApplyInterestEventPublisher.java
@@ -1,13 +1,15 @@
-package com.imt.projet.Banque.interfaces.events.Contrats;
+package com.imt.projet.Banque.infrastructure.events.Contrats;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.imt.projet.Banque.interfaces.events.Contrats.ApplyInterestEvent;
+
 @Component
 public class ApplyInterestEventPublisher {
 
-    private static final int TIME = 60000/4; 
+    private static final int TIME = 15*1000; // Dans ce cas, toutes les 15 secondes
     private final ApplicationEventPublisher eventPublisher;
 
     public ApplyInterestEventPublisher(ApplicationEventPublisher eventPublisher) {
